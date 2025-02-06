4abe967efe9b84c013b6755cf0122d2d6c42a4b0
Trevor Harris <tharris@tamu.edu>
2025-02-06 12:52:09 -0500

    sketched out a few more figure 1 ideas / apprendix fodder

--- methods/lsci.py
+++ methods/lsci.py
@@ -41,37 +41,46 @@ def local_tukey(rval, q, weight):
     return 2 * jnp.min(jnp.array([lcdf_l, lcdf_r]))
 local_tukey = jit(local_tukey)
 local_tukey_vmap = vmap(local_tukey, (1, 1, None))
-
 local_tukey_self = vmap(local_tukey, (None, 0, None))
 local_tukey_self = jit(vmap(local_tukey_self, (1, 1, None)))
 
-def warped_unif(rng, a, b):
-    u = jax.random.uniform(rng)
-    return (b - a) * u + a
 
-def conformal_resample(rng, x, w):
-    phi = local_quantile(x, warped_unif(rng, 0.0, 1.0), w)
+def random_phi(rng, rval, weight):
+    phi = local_quantile(rval, jax.random.uniform(rng), weight)
     return phi
-conformal_resample = vmap(conformal_resample, (0, 1, None))
+random_phi = vmap(random_phi, (0, 1, None))
 
-def conformal_ens(rng, x, w):
-    rngs = random.split(rng, x.shape[1])
-    return conformal_resample(rngs, x, w)
-conformal_ens = vmap(conformal_ens, (0, None, None))
+def phi_ensemble(rng, rval, weight):
+    rngs = random.split(rng, rval.shape[1])
+    return random_phi(rngs, rval, weight)
+phi_ensemble = vmap(phi_ensemble, (0, None, None))
 
-def lsci_sample(residuals, local_weight, pca_state, alpha, nsamp, rng = random.PRNGKey(0)):
-    res_proj = pcax.transform(pca_state, residuals)
+def lsci_ensemble(rval, weight, pca_state, alpha, nsamp, rng = random.PRNGKey(0)):
+    rval_proj = pcax.transform(pca_state, rval)
     rngs = random.split(rng, nsamp)
-    phi_t = conformal_ens(rngs, res_proj, local_weight)
+    phi_t = phi_ensemble(rngs, rval_proj, weight)
     ens_t = pcax.recover(pca_state, phi_t)
     
-    dval = jnp.mean(local_tukey_self(res_proj, res_proj, local_weight), axis = 0)
+    dval = jnp.mean(local_tukey_self(rval_proj, rval_proj, weight), axis = 0)
     qval = jnp.quantile(dval, alpha)
-    dphi = jnp.mean(local_tukey_self(res_proj, phi_t, local_weight), axis = 0)
+    dphi = jnp.mean(local_tukey_self(rval_proj, phi_t, weight), axis = 0)
     return ens_t[dphi > qval]
 
-def lsci_band(residuals, local_weight, pca_state, alpha, nsamp, rng = random.PRNGKey(0)):
-    lsci_conf = lsci_sample(residuals, local_weight, pca_state, alpha, nsamp, rng)
+def lsci_band(rval, weight, pca_state, alpha, nsamp, rng = random.PRNGKey(0)):
+    lsci_conf = lsci_ensemble(rval, weight, pca_state, alpha, nsamp, rng)
     lsci_lower = jnp.min(lsci_conf, axis = 0)
     lsci_upper = jnp.max(lsci_conf, axis = 0)
     return lsci_lower, lsci_upper
+
+def phi_state(yval, yval_hat, nproj):
+    yval = yval.reshape(yval.shape[0], -1)
+    yval_hat = yval_hat.reshape(yval_hat.shape[0], -1)
+    rval = yval - yval_hat
+    return pcax.fit(rval, n_components=nproj)
+
+def lsci(rval, xval, xtest, pca_state, alpha, gamma, nsamp):
+    xval = xval.squeeze()
+    rval = rval.squeeze()
+    xtest = xtest.squeeze()[None,]
+    lweight = local_weights(xval, xtest, gamma)[0]
+    return lsci_band(rval, lweight, pca_state, alpha, nsamp)
